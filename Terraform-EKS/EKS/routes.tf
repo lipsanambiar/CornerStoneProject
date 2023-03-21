98df44b3f4ebc1bc8616b13b47a600404e14d023
lipsanambiar <120271656+lipsanambiar@users.noreply.github.com>
2023-03-21 10:36:21 -0500

    Update routes.tf

--- Terraform-EKS/EKS/routes.tf
+++ Terraform-EKS/EKS/routes.tf
@@ -1,7 +1,8 @@
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  route {
+  route = [
+    {
       cidr_block                 = "0.0.0.0/0"
       nat_gateway_id             = aws_nat_gateway.nat.id
       carrier_gateway_id         = ""
@@ -15,7 +16,8 @@ resource "aws_route_table" "private" {
       transit_gateway_id         = ""
       vpc_endpoint_id            = ""
       vpc_peering_connection_id  = ""
-  }
+    },
+  ]
 
   tags = {
     Name = "private"
@@ -25,13 +27,14 @@ resource "aws_route_table" "private" {
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
-  route {
+  route = [
+    {
       cidr_block                 = "0.0.0.0/0"
-      nat_gateway_id             = aws_nat_gateway.nat.id
+      gateway_id                 = aws_internet_gateway.igw.id
+      nat_gateway_id             = ""
       carrier_gateway_id         = ""
       destination_prefix_list_id = ""
       egress_only_gateway_id     = ""
-      gateway_id                 = ""
       instance_id                = ""
       ipv6_cidr_block            = ""
       local_gateway_id           = ""
@@ -39,14 +42,16 @@ resource "aws_route_table" "public" {
       transit_gateway_id         = ""
       vpc_endpoint_id            = ""
       vpc_peering_connection_id  = ""
-  }
+    },
+  ]
 
   tags = {
     Name = "public"
   }
 }
-resource "aws_route_table_association" "private-us-west-1a" {
-  subnet_id      = aws_subnet.private-us-west-1a.id
+
+resource "aws_route_table_association" "private-us-west-1c" {
+  subnet_id      = aws_subnet.private-us-west-1c.id
   route_table_id = aws_route_table.private.id
 }
 
@@ -55,8 +60,8 @@ resource "aws_route_table_association" "private-us-west-1b" {
   route_table_id = aws_route_table.private.id
 }
 
-resource "aws_route_table_association" "public-us-west-1a" {
-  subnet_id      = aws_subnet.public-us-west-1a.id
+resource "aws_route_table_association" "public-us-west-1c" {
+  subnet_id      = aws_subnet.public-us-west-1c.id
   route_table_id = aws_route_table.public.id
 }
 
