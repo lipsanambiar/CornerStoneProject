f8ff1d343d3f7e06ec040f74399cc5e4a82b82a7
lipsanambiar <120271656+lipsanambiar@users.noreply.github.com>
2023-03-21 10:34:57 -0500

    Update provider.tf

--- Terraform-EKS/EKS/provider.tf
+++ Terraform-EKS/EKS/provider.tf
@@ -1,13 +1,12 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.58.0"
+      version = "~> 3.76.1"
     }
   }
-}
-
-provider "aws" {
-  # Configuration options
-  region = "us-west-1"
 }
